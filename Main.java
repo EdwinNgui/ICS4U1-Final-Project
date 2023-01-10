2be932167faff599bf997dc452eedf931130eb9d
Edwin <edwin.nguim@gmail.com>
2023-01-09 23:02:28 -0500

    now works on laptop

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         System.out.println("hi");
         System.out.println("this is a new test addition");
         System.out.println("this is a test from the second pc");
+        System.out.println("this is a new portion");
     }
 
 }
