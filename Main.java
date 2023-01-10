53f70953d9610a04157d8b63a8cbddba0d07a491
Edwin <edwin.nguim@gmail.com>
2023-01-09 22:14:34 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
 	public static void main(String[] args) {
         System.out.println("hi");
         System.out.println("this is a new test addition");
+        System.out.println("this is a test from the second pc");
     }
 
 }
