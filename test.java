694946750db7d1984e3cef9f4a8aa2c673bc3eba
Edwin <edwin.nguim@gmail.com>
2023-01-16 09:09:16 -0500

    intro improve

---
+++ test.java
@@ -0,0 +1,11 @@
+
+public class test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Normal words and then this happens");
+        System.out.println("🅐");
+
+    }
+}
+
